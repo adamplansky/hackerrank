2faafb74751e990260677760c319665b28d17228
Adam Plansky <adamplansky@gmail.com>
2016-11-03 22:08:47 +0100

    jim-and-the-orders refactored

--- jim-and-the-orders/jim-and-the-orders.rb
+++ jim-and-the-orders/jim-and-the-orders.rb
@@ -1,17 +1,6 @@
 N = gets.strip.to_i
 ary = []
-N.times{  ary << gets.strip.split(" ").map(&:to_i).inject{|a,b| a + b} }
-max_index = -1
-res_ary = []
-for i in 0...N
-  max_val = Float::INFINITY
-  for j in 0...N
-    if ary[j] < max_val
-      max_index = j
-      max_val = ary[j]
-    end
-  end
-  ary[max_index] = Float::INFINITY
-  res_ary << max_index+1
+N.times do |index|
+  ary << [gets.strip.split(" ").map(&:to_i).inject{|a,b| a + b}, index+1]
 end
-puts res_ary.join(' ')
+puts ary.sort.map{|val,index|index}.join(" ")
